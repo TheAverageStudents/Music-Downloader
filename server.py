e494cb7f5a568dd9bc550eb9458ba187cbf70daf
TheAverageStudents <70644006+TheAverageStudents@users.noreply.github.com>
2022-02-03 11:18:48 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import spotipy, flask, json
+import spotipy, flask
 
 a = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyClientCredentials(client_id="", client_secret=""))
 s = flask.Flask('')
@@ -13,15 +13,6 @@ def t():
 def p():
     i = flask.request.url.split("?i=")[1]
     x = a.playlist(i)
-    '''
-    ["tracks"]["items"]
-    print(x[0]["track"]["duration_ms"])
-    o = {}
-    c = []
-    for j in x:
-        c.append(str(j["track"]["name"] + " " + j["track"]["album"]["artists"][0]["name"]))
-        #print(j["duration_ms"])
-    '''
     return x
 
 s.run(host="0.0.0.0")
